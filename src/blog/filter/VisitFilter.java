bd228e226517809a16415188ad7252674821d457
liuyib <1656081615@qq.com>
2019-11-07 10:58:45 +0800

    feat: 首页样式改版

--- src/blog/filter/VisitFilter.java
+++ src/blog/filter/VisitFilter.java
@@ -1,12 +1,10 @@
 package blog.filter;
 
 import java.io.IOException;
-import java.util.Date;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -14,9 +12,6 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import blog.db.VisitorDB;
-import blog.utils.DateUtils;
 
 //仅统计首页jsp的访问量
 @WebFilter(filterName = "VisitFilter", urlPatterns = { "*.jsp" })
@@ -50,15 +45,15 @@ public class VisitFilter implements Filter {
 
 					Thread t = new Thread(new Runnable() {
 						public void run() {
-							// 向数据库写入信息
-							VisitorDB.visit(rq);
-
-							// 发送新的cookie
-							Cookie c = new Cookie("myblog_visitor", DateUtils.getFormatDate(new Date()));
-							// cookie生命周60分钟
-							c.setMaxAge(60 * 60);
-							c.setPath("/Blog");
-							rp.addCookie(c);
+//							// 向数据库写入信息
+//							VisitorDB.visit(rq);
+//
+//							// 发送新的cookie
+//							Cookie c = new Cookie("myblog_visitor", DateUtils.getFormatDate(new Date()));
+//							// cookie生命周60分钟
+//							c.setMaxAge(60 * 60);
+//							c.setPath("/Blog");
+//							rp.addCookie(c);
 						}
 					});
 					t.start();
