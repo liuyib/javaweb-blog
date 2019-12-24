4fb6a5750895c2108832d8eab1368a0c4145c4a0
liuyib <1656081615@qq.com>
2019-12-24 17:49:39 +0800

    fix: 解决站点访问量统计不生效的问题

--- src/blog/filter/VisitFilter.java
+++ src/blog/filter/VisitFilter.java
@@ -1,6 +1,7 @@
 package blog.filter;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -13,8 +14,11 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import blog.db.VisitorDB;
+import blog.utils.DateUtils;
+
 //仅统计首页jsp的访问量
-@WebFilter(filterName = "VisitFilter", urlPatterns = { "*.jsp" })
+@WebFilter(filterName = "VisitFilter", urlPatterns = { "*" })
 public class VisitFilter implements Filter {
 
 	public void destroy() {
@@ -27,37 +31,32 @@ public class VisitFilter implements Filter {
 		HttpServletRequest rq = (HttpServletRequest) request;
 		HttpServletResponse rp = (HttpServletResponse) response;
 
-		if (rq.getRequestURL().indexOf("index.jsp") != -1) {
-
-			synchronized (this) {
-				// System.out.println(rq.getRequestURI());
-				Cookie[] cookies = rq.getCookies();
-				boolean visited = false;
-				if (cookies != null) {
-					for (Cookie cookie : cookies) {
-						if (cookie.getName().equals("myblog_visitor")) {
-							visited = true;
-							break;
-						}
+		synchronized (this) {
+			Cookie[] cookies = rq.getCookies();
+			boolean visited = false;
+			if (cookies != null) {
+				for (Cookie cookie : cookies) {
+					if (cookie.getName().equals("myblog_visitor")) {
+						visited = true;
+						break;
 					}
 				}
-				if (!visited) {
+			}
+			if (!visited) {
+				Thread t = new Thread(new Runnable() {
+					public void run() {
+						// 向数据库写入信息
+						VisitorDB.visit(rq);
 
-					Thread t = new Thread(new Runnable() {
-						public void run() {
-//							// 向数据库写入信息
-//							VisitorDB.visit(rq);
-//
-//							// 发送新的cookie
-//							Cookie c = new Cookie("myblog_visitor", DateUtils.getFormatDate(new Date()));
-//							// cookie生命周60分钟
-//							c.setMaxAge(60 * 60);
-//							c.setPath("/Blog");
-//							rp.addCookie(c);
-						}
-					});
-					t.start();
-				}
+						// 发送新的cookie
+						Cookie c = new Cookie("myblog_visitor", DateUtils.getFormatDate(new Date()));
+						// cookie生命周60分钟
+						c.setMaxAge(60 * 60);
+						c.setPath("/Blog");
+						rp.addCookie(c);
+					}
+				});
+				t.start();
 			}
 		}
 		chain.doFilter(request, response);
